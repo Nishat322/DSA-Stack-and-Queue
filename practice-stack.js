f5f5533bf5b4af88fb7be9b7c82c154f34e1b218
Nishat Hoque <hoquen0397@gmail.com>
2021-03-02 12:11:21 -0500

    palindrome

--- practice-stack.js
+++ practice-stack.js
@@ -7,15 +7,18 @@ starTreck.push('Spock')
 starTreck.push('McCoy')
 starTreck.push('Scotty')
 
+/*************************************** USEFUL METHODS FOR A STACK ******************************************************/
 function peek(stack){
     const node = stack.top
     console.log(node.data)
 }
-peek(starTreck)
+
+//peek(starTreck)
 
 function isEmpty(stack){
     if(stack.top == null){
         console.log('stack is empty')
+        return null
     } 
     else (
         console.log('stack is not empty')
@@ -23,16 +26,44 @@ function isEmpty(stack){
 }
 
 const emptyStack = new Stack()
-isEmpty(starTreck)
-isEmpty(emptyStack)
+//isEmpty(starTreck)
+//isEmpty(emptyStack)
 
 function display(stack){
     let node = stack.top
     while (node !== null){
         console.log(node.data)
-        stack.pop()
-        node = stack.top 
+        node = node.next
     }
 }
 
-display(starTreck)
+//display(starTreck)
+
+/*************************************************** CHECKING FOR PALINDROMES USING A STACK *******************************************/
+function is_palindrome(s){
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+    const stack = new Stack()
+    for(let i = 0; i< s.length; i++){
+        stack.push(s.charAt(i))
+    }
+
+    let reverseString = ''
+    let node = stack.top
+    while(node !== null){
+        reverseString = reverseString + stack.pop()
+        node = node.next
+    }
+
+    if( s === reverseString){
+        console.log('String is a palindrome')
+    }
+    else{
+        console.log('String is not a palindrome')
+    }
+    
+}
+
+is_palindrome("dad")
+is_palindrome("A man, a plan, a canal: Panama")
+is_palindrome("1001")
+is_palindrome("Tauhida")
